afc62cc4cc60604c12eeddb95784af219d3d59c3
linjunJR <linjrjun@gmail.com>
2025-06-12 15:28:16 +0800

    edit loader

--- loader.py
+++ loader.py
@@ -6,30 +6,6 @@ import re
 from mrcnn.config import Config
 from mrcnn.utils import Dataset
 
-############################################################
-#  Configurations
-############################################################
-
-class DiskConfig(Config):
-    NAME = "disk" # Name the configurations. For example, 'COCO', 'Experiment 3', ...etc.
-    NUM_CLASSES = 1 + 2  # background + binary disk
-    GPU_COUNT = 1     # NUMBER OF GPUs to use. When using only a CPU, this needs to be set to 1.
-    IMAGES_PER_GPU = 1 # Number of images to train with on each GPU. A 12GB GPU can typically handle 2 images of 1024x1024px.
-    STEPS_PER_EPOCH = 100
-    DETECTION_MIN_CONFIDENCE = 0.8
-    VALIDATION_STEPS = 50
-    
-    # Backbone network architecture
-    # Supported values are: resnet50, resnet101.
-    # You can also provide a callable that should have the signature
-    # of model.resnet_graph. If you do so, you need to supply a callable
-    # to COMPUTE_BACKBONE_SHAPE as well
-    BACKBONE = "resnet101"
-
-    # Only useful if you supply a callable to BACKBONE. Should compute
-    # the shape of each layer of the FPN Pyramid.
-    # See model.compute_backbone_shapes
-    COMPUTE_BACKBONE_SHAPE = None
 
 ############################################################
 #  Dataset
@@ -43,8 +19,8 @@ class DiskDataset(Dataset):
         # Train or validation dataset?
         assert subset in ["train", "val"]
         
-        images_dir = os.path.join(dataset_dir, "images", subset)
-        masks_dir = os.path.join(dataset_dir, "masks", subset)
+        images_dir = os.path.join(dataset_dir, subset, "images")
+        masks_dir = os.path.join(dataset_dir, subset, "masks")
 
         for filename in os.listdir(images_dir):
             image_id = os.path.splitext(filename)[0]
